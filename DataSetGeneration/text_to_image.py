db77699a9761dad3e647a9640613fa0570c6d189
Shane Miller <shanekmiller44@gmail.com>
2021-01-05 10:50:37 -0500

    Changed image pathing and size

--- DataSetGeneration/text_to_image.py
+++ DataSetGeneration/text_to_image.py
@@ -8,12 +8,12 @@ import os
 
 #Make folder to save images
 current_file_path = pathlib.Path(__file__).parent.absolute()
-path = current_file_path / 'images'
+path = current_file_path.parents[0] / 'input_images' / 'images'
 shutil.rmtree(path, ignore_errors=True)
 os.mkdir(path)
 
 # Open kanji list
-file = open('kanji.txt', 'r', encoding='utf8')
+file = open(current_file_path / 'kanji.txt', 'r', encoding='utf8')
 
 # Create list of lines
 lines = file.readlines()
@@ -21,10 +21,10 @@ lines = file.readlines()
 # Loop through each kanji
 for char_num, char in enumerate(tqdm(lines, desc="Generating Images"), 1):
     # Create image
-    img = Image.new('RGB', (128, 128), (255, 255, 255))
+    img = Image.new('RGB', (256, 256), (255, 255, 255))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(str(current_file_path / 'NotoSansJP-Regular.otf'), 100)
-    draw.text((15, -15), char.strip(), fill='black', font=font, align='center')
+    font = ImageFont.truetype(str(current_file_path / 'NotoSansJP-Regular.otf'), 225)
+    draw.text((15, -50), char.strip(), fill='black', font=font, align='center')
 
     # Save image
     img.save(path / (str(char_num) + '.png'), 'PNG')
